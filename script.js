470ec14d37b945c3c7cff9fb05e2b23503926b25
atrari.pro <atrari.pro@gmail.com>
2020-11-11 20:28:44 +0100

    add click event to box-hour

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
+function renderHourDigit(hour) {
+    return `0${hour}`.slice(-2);
+}
+
 function renderHourTemplate(hour) {
     return `
     <div class="hour">
-        <div>${hour}</div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
+        <div class="hour-label">${renderHourDigit(hour)}</div>
+        <div class="hour-box"></div>
+        <div class="hour-box"></div>
+        <div class="hour-box"></div>
+        <div class="hour-box"></div>
+        <div class="hour-box"></div>
+        <div class="hour-box"></div>
     </div>
     `;
 }
+
+
+
+
 
 let container = document.getElementById("container");
 for (let i=0; i<24; i++){
     container.innerHTML += renderHourTemplate(i);
 }
+
+/*function switchColor(div) {
+    div.style.background ='grey';
+}
+
+let divs =document.querySelectorAll('.hour-box');
+divs.forEach(item => switchColor(item));
+
+/*  c'est meme chose que en dessus !!! :
+for(let i=0; i<divs.length; i++) {
+    switchColor(divs[i]);
+}*/
+
+function switchColor(div) {
+    let background = div.style.background;
+    if(background === 'white' || background === '') {
+      background = 'grey'
+    } else {
+      background = 'white'
+    }
+    div.style.background = background;
+  }
+  
+  function addClickEventToHourBox() {
+    let divs = document.querySelectorAll('.hour-box');
+    divs.forEach(
+      item => 
+      item.addEventListener('click', () => switchColor(item))
+    );
+  }
+  
+  document.addEventListener('DOMContentLoaded', addClickEventToHourBox);
